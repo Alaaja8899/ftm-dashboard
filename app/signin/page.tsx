752bdba95e17be8d8fd441fe66786a522efb1883
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2025-02-10 11:08:16 +0300

    sign in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { TreePalmIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 function Page() {
@@ -9,6 +13,29 @@ function Page() {
         <div className="top w-full flex justify-end">
           <ModeToggle />
         </div>
+
+          {/* login form */}
+
+          <div className="login-container flex justify-center mt-20">
+
+              <form className="border p-6  md:w-[35rem] w-full rounded-lg flex flex-col space-y-4">
+                <h2 className="flex gap-1 font-bold text-2xl">Family <TreePalmIcon/> Tree </h2>
+                <p className="text-gray-500">Log in Access your family tree dashboard</p>
+                <Input placeholder="Email / Username" required/>
+                <Input placeholder="Email / Username" required type="password"/>
+                <Button>Sign in </Button>
+
+                <Link href={'/'}>
+                Forget Password ?
+                </Link>
+
+              </form>
+
+
+          </div>
+
+
+
       </main>
     </div>
   );
