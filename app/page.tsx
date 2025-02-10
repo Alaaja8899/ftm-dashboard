1285e68eac9156ae05673e7b21fdf814495cec05
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2025-02-10 08:10:22 +0300

    never use it fail fixation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,35 @@
+import CountCards from "@/components/CountCards";
 import { ModeToggle } from "@/components/ModeToggle";
+import { User, User2Icon, UserCheck, UserMinus } from "lucide-react";
+
+
+const cardItems = [
+  {
+    name:"Number of all Boys",
+    desc: "Latest changed 1 week ago",
+    count: 43, 
+    icon: User, 
+  },
+  {
+    name:"Number of girls",
+    desc: "Latest changed 1 day ago",
+    count: 10, 
+    icon: User2Icon, 
+  },
+  {
+    name: "Alive people",
+    desc: "Latest modified 2min ago",
+    count: 50, 
+    icon: UserCheck, 
+  },
+  {
+    name: "Deseaced people",
+    desc: "Latest modified 1month ago",
+    count:3, 
+    icon: UserMinus, 
+  },
+]
+
 
 export default function Home() {
   return (
@@ -8,9 +39,16 @@ export default function Home() {
           <h2>
             Dashboard
           </h2>
+
+          
           <ModeToggle/>
         </div>
-
+        <div className="count-cards px-6 flex gap-2 md:flex-row flex-col">
+          {cardItems.map(info=>{
+            return <CountCards key={info.name} name={info.name} desc={info.desc} icon={info.icon} count={info.count} />
+          })}
+        </div>
+        
         
       </main>
     </div>
---
+++ components/CountCards.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from "react";
+import { LucideIcon } from "lucide-react";
+
+type CountCardsProps = {
+  name: string;
+  count: number;
+  icon: LucideIcon; // Define as a Lucide icon type
+  desc: string;
+};
+
+const CountCards: React.FC<CountCardsProps> = ({ name, count, icon: Icon, desc }) => {
+  return (
+    <div className="border w-full p-3 px-6 md:w-[16rem] rounded-sm shadow-sm flex-col space-y-4">
+      <div className="top flex justify-between">
+        <h3 className="font-bold">{name}</h3>
+        <div className="icon">
+          <Icon className="w-6 h-6" /> {/* Render the component */}
+        </div>
+      </div>
+      <h2 className="count-number font-bold text-3xl">{count}</h2>
+      <p className="small-desc text-gray-500">{desc}</p>
+    </div>
+  );
+};
+
+export default CountCards;
